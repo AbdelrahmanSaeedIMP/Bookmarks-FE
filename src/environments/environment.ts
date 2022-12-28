4817556cece98272910750926d135ea5b237145d
AbdelrahmanSaeed11 <abdelrahmansaeed2101@gmail.com>
2022-12-28 18:35:00 +0200

    add api url root and safe saved token for test to environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'http://localhost:5000/temp-project-7e3d5/us-central1/app',
+  savedToken: [
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEiLCJuYW1lIjoic2FlZWQifQ.IG0B2kTFoe0-ok0oL6mUIWb1v0ya9-d6NqDu42HccKo',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjIiLCJuYW1lIjoibWFobW91ZCJ9.h1HRBtj0R_UZ0CIWsyZ4HNXi9s9wOiBcOT6exT5YzBA',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjMiLCJuYW1lIjoiaHVzc2luIn0.xUtCfanPLY3moSahLI0w0D4NNgLlUixcWR2NBJRC0rU'
+  ]
 };
 
 /*
